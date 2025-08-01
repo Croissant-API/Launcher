0e791dfb79c0503e6e0864b3eb3d94cd5fc4fab9
fox3000foxy <fox3000foxy@gmail.com>
2025-08-01 15:46:59 +0200

    Remove preload script, update index.js to create desktop shortcut directly, and delete webpack configuration file.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-(async () => {
-  const path = require('path');
-  const ws = require('windows-shortcuts');
 
-  // Utilisation du dossier Bureau de l'utilisateur sans Electron
-  const desktop = path.join(require('os').homedir(), 'Desktop');
-  console.log(`Creating shortcut on desktop: ${desktop}`);
-  const exePath = process.execPath;
-  const shortcutPath = path.join(desktop, 'Croissant Launcher.lnk');
+const path = require('path');
+const ws = require('windows-shortcuts');
 
-  ws.create(shortcutPath, {
-    target: exePath,
-    desc: 'Start Croissant Launcher',
-    icon: exePath,
-  });
-  await import("./app/app.js");
-})();
+// Utilisation du dossier Bureau de l'utilisateur sans Electron
+const desktop = path.join(require('os').homedir(), 'Desktop');
+console.log(`Creating shortcut on desktop: ${desktop}`);
+const exePath = process.execPath;
+const shortcutPath = path.join(desktop, 'Croissant Launcher.lnk');
+
+ws.create(shortcutPath, {
+  target: exePath,
+  desc: 'Start Croissant Launcher',
+  icon: exePath,
+});
+require('./app/app.js'); // ou le point d'entrée réel
