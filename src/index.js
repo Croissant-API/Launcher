862a1eacedd4a23010fd8f9ae7ee654b45d7dd20
fox3000foxy <fox3000foxy@gmail.com>
2025-08-29 22:49:53 +0200

    Refactor index.js to comment out unused updateElectronApp functionality and clean up code structure

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-import { updateElectronApp } from 'update-electron-app';
-updateElectronApp();
-
-import { startApp } from "./app/app.js";
-startApp();
+// import { updateElectronApp } from 'update-electron-app';
+// updateElectronApp();
 
 import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// const exePath = process.execPath;
-// createDesktopShortcut({
-//     windows: {
-//         filePath: exePath,
-//         name: 'Croissant Launcher',
-//         comment: 'Open Croissant Launcher',
-//         icon: __dirname + '/icon.ico', // chemin vers ton fichier .ico
-//         desktop: true,
-//         startMenu: true,
-//         menu: true // Ajoute le raccourci dans le menu démarrer
-//     }
-// });
-
 import ws from 'windows-shortcuts';
 import { app } from 'electron';
+import { startApp } from "./app/app.js";
+startApp();
 
 const exePath = process.execPath;
 const desktop = app.getPath('desktop');
